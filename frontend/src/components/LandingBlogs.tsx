10342e27a5cb92d2ef79404526b507ef28448bf0
premchandellendula <premcellendula@gmail.com>
2024-07-19 16:04:46 +0530

    landing

--- frontend/src/components/LandingBlogs.tsx
+++ frontend/src/components/LandingBlogs.tsx
@@ -8,29 +8,29 @@ interface BlogCardInputs {
     "publishedDate": string
 }
 
-export const LandingBlogs = ({id, authorName, title, content, publishedDate}: BlogCardInputs) => {
-    return <Link to={`/blog/${id}`}>
+export const LandingBlogs = ({blog}: {blog: BlogCardInputs}) => {
+    return <Link to={`/sample/${blog.id}`}>
         <div className="border-b-2 border-gray-100 pb-3 p-3 w-screen max-w-screen-md cursor-pointer">
             <div className="flex">
-            <Avatar name={authorName} /> 
+            <Avatar name={blog.authorName} /> 
             <div className="font-extralight pl-2 text-sm flex flex-col justify-center">
-                    {authorName} • 
+                    {blog.authorName} • 
                 </div>
                 <div className="pl-2 font-thin text-gray-500 text-sm flex flex-col justify-center">
-                    {publishedDate}
+                    {blog.publishedDate}
                 </div>
             </div>
 
             <div className="text-xl font-semibold pt-2">
-                {title}
+                {blog.title}
             </div>
 
             <div className="text-md font-normal">
-                {content.slice(0, 100) + "..."}
+                {blog.content.slice(0, 100) + "..."}
             </div>
 
             <div className="text-gray-600 text-sm font-normal pt-3">
-                {`${Math.ceil(content.length / 100)} mins read`}
+                {`${Math.ceil(blog.content.length / 100)} mins read`}
             </div>
         </div>
     </Link>
@@ -41,4 +41,58 @@ export function Avatar({ name } : {name : string}){
         <span className="text-sm  text-gray-600 dark:text-gray-300">{name[0]}</span>
     </div>
     
+}
+
+
+
+// import { Appbar } from "./Appbar"
+
+interface FullBlogProps {
+    id: string,
+    authorName: string,
+    title: string,
+    content: string,
+    publishedDate: string
+}
+
+export const FullLandingBlogs = ({blog}: {blog: FullBlogProps}) => {
+    return <div>
+        <div className="flex justify-center">
+            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
+                <div className="col-span-8">
+                    <div className="text-3xl font-extrabold">
+                        {blog.title}
+                    </div>
+                    <div className="text-gray-600 pt-2">
+                        Posted on June 22, 2024
+                    </div>
+                    <div className="text-xl font-normal pt-4 text-gray-800">
+                        {blog.content}
+                    </div>
+                </div>
+                <div className="col-span-4">
+                    <div className="text-gray-700">
+                        Author
+                    </div>
+
+                    <div className="flex">
+                        <div className="pr-3 flex flex-col justify-center">
+                            <div className="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                                <span className="text-md text-gray-600 dark:text-gray-300">U</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="text-xl font-bold">
+                                {blog.authorName}
+                            </div>
+                            <div className="pt-2 text-gray-700">
+                                Random sentence just to fill the desc of the author
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 }
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -7,44 +7,66 @@
 // }
 
 
+import { useState } from "react";
 import { LandingAppbar } from "../components/LandingAppbar";
 import { LandingBlogs } from "../components/LandingBlogs";
 
 export const Landing = () => {
-    // const {loading, blogs} = useBlogs();
+    const [blogs, setBlogs] = useState([
+        {
+            id: "1",
+            authorName: "Prahladh",
+            title: "A detailed blog on Web Dev roadmap",
+            content: "This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too",
+            publishedDate: "22 Jun, 2024"
+        },
+        {
+            id: "2",
+            authorName: "Prahladh",
+            title: "A detailed blog on Web Dev roadmap",
+            content: "This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too",
+            publishedDate: "22 Jun, 2024"
+        },
+        {
+            id: "3",
+            authorName: "Prahladh",
+            title: "A detailed blog on Web Dev roadmap",
+            content: "This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too",
+            publishedDate: "22 Jun, 2024"
+        },
+        {
+            id: "4",
+            authorName: "Prahladh",
+            title: "A detailed blog on Web Dev roadmap",
+            content: "This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too",
+            publishedDate: "22 Jun, 2024"
+        },
+        {
+            id: "5",
+            authorName: "Prahladh",
+            title: "A detailed blog on Web Dev roadmap",
+            content: "This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too",
+            publishedDate: "22 Jun, 2024"
+        }
 
-    // if(loading){
-    //     return <div>
-    //         <LandingAppbar />
-    //         <div className="flex justify-center">
-    //             <div>
-    //                 <BlogSkeleton />
-    //                 <BlogSkeleton />
-    //                 <BlogSkeleton />
-    //                 <BlogSkeleton />
-    //                 <BlogSkeleton />
-    //             </div>
-    //         </div>
-    //     </div>
-    // }
-
+    ])
     return <div>
         <LandingAppbar />
         <div className="flex justify-center">
             <div className="flex flex-col justify-center">
-                {/* {blogs.map(blog => <BlogCard key={blog.id} id={blog.id} authorName={blog.author.name || "Anonymous User"} title={blog.title} content={blog.content} publishedDate={"22 Jun, 2024"} />)} */}
+                {blogs.map(blog => <LandingBlogs key={blog.id} blog={blog}/>)}
                 
                 {/* <BlogCard key={"22"} id={"22"} authorName={"Anonymous User"} title={"This is a sample title"} content={"This is a sample content for the above title which was provided the great ksfkjkjf"} publishedDate={"22 Jun, 2024"} /> */}
 
-                <LandingBlogs key={"1"} id={"1"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
+                {/* <LandingBlogs key={"1"} id={"1"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
 
-                <LandingBlogs key={"1"} id={"1"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
+                <LandingBlogs key={"2"} id={"2"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
 
-                <LandingBlogs key={"1"} id={"1"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
+                <LandingBlogs key={"3"} id={"3"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
 
-                <LandingBlogs key={"1"} id={"1"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
+                <LandingBlogs key={"4"} id={"4"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
 
-                <LandingBlogs key={"1"} id={"1"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />            
+                <LandingBlogs key={"5"} id={"5"} authorName={"Prahladh"} title={"A detailed blog on Web Dev roadmap"} content={"This is a detailed blog on the web dev roadmap using MERN stack, and includes jwt authentication and cloudflares workers too"} publishedDate={"22 Jun, 2024"} />             */}
             </div>
         </div>
     </div>
